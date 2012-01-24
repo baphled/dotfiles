08028384ae89ea0076b572ec3b3422f2f7260e53
Yomi Colledge <baphled@boodah.net>
2012-01-24 15:28:47 +0000

    Braid: Update mirror 'oh-my-zsh' to '466b904'

--- oh-my-zsh/plugins/git/git.plugin.zsh
+++ oh-my-zsh/plugins/git/git.plugin.zsh
@@ -17,6 +17,7 @@ alias gca='git commit -v -a'
 compdef _git gca=git-commit
 alias gco='git checkout'
 compdef _git gco=git-checkout
+alias gcm='git checkout master'
 alias gb='git branch'
 compdef _git gb=git-branch
 alias gba='git branch -a'
@@ -35,6 +36,8 @@ alias ga='git add'
 compdef _git ga=git-add
 alias gm='git merge'
 compdef _git gm=git-merge
+alias grh='git reset HEAD'
+alias grhh='git reset HEAD --hard'
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
